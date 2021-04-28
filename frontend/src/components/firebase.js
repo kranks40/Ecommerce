f8201d7f3050fb3da7d14395bcb17307c95d3256
kranks40 <eng_peters20@yahoo.com>
2021-04-28 03:23:03 -0400

    Installed firebase

---
+++ frontend/src/components/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGWqB6op8nkmOfRD5SLEqF7aaIfRcRiTQ",
+  authDomain: "surebuy-310211.firebaseapp.com",
+  projectId: "surebuy-310211",
+  storageBucket: "surebuy-310211.appspot.com",
+  messagingSenderId: "439729712126",
+  appId: "1:439729712126:web:ae15a7ec387a120590eeaf",
+  measurementId: "G-J2QJ67KY7Q",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+
+export { db };
