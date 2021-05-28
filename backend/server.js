37469b6ac42a8c329fe7d57721b5176c6b754bfe
kranks40 <eng_peters20@yahoo.com>
2021-05-28 10:31:52 -0400

    server changes

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
 
 app.get("/api/config/paypal", (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
 
 app.get("/api/config/google", (req, res) => {
