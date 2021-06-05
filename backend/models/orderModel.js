e3a2eff94a121a96c8a34269d55a590e1984617f
kranks40 <eng_peters20@yahoo.com>
2021-06-05 12:36:11 -0400

    Replaced ID for Id in module

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -39,7 +39,7 @@ const orderSchema = new mongoose.Schema(
     totalPrice: { type: Number, required: true },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     seller: {
-      type: mongoose.Schema.Types.ObjectID,
+      type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -14,7 +14,7 @@ const reviewSchema = new mongoose.Schema(
 const productSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, unique: true },
-    seller: { type: mongoose.Schema.Types.ObjectID, ref: "User" },
+    seller: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     image: { type: String, required: true },
     brand: { type: String, required: true },
     category: { type: String, required: true },
