2b07bd968d444d3f208283848bfb19c340fed85e
kranks40 <eng_peters20@yahoo.com>
2021-06-05 12:41:10 -0400

    Changed model order

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -14,15 +14,15 @@ const reviewSchema = new mongoose.Schema(
 const productSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, unique: true },
-    seller: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    image: { type: String, required: true },
-    brand: { type: String, required: true },
     category: { type: String, required: true },
-    description: { type: String, required: true },
+    image: { type: String, required: true },
     price: { type: Number, required: true },
     countInStock: { type: Number, required: true },
+    brand: { type: String, required: true },
     rating: { type: Number, required: true },
     numReviews: { type: Number, required: true },
+    description: { type: String, required: true },
+    seller: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     reviews: [reviewSchema],
   },
   {
